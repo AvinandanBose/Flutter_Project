d9c2a3625d800d9805d56fc270932b75a54aef79
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-09-17 09:46:51 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+      ),
+    ));
