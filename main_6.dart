446e62c2fc8c0e383a2aecd440a4b55162aed4f5
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-09-17 22:01:55 +0530

    Add files via upload

---
+++ main_6.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+          backgroundColor: Colors.red[700],
+        ),
+        body: Center(
+          child: Text(
+            'Hello Its My First App',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[700],
+                fontFamily: 'Italianno'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Text('Click'),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
+    ));
