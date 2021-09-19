8a175ea71cff9e31b273f958a899e7abaf432e1d
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-09-19 18:50:43 +0530

    Add files via upload

---
+++ main_11.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Test()));
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+      body: Center(child: Image.asset('assets/Elon-1.jpg')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
